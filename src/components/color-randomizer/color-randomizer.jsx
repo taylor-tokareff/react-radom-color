74a2d8f02a38dc9adb119e09d6771a832927375a
taylor-tokareff <taylortokareff@gmail.com>
2021-07-26 16:59:50 -0700

    got some colors rendering

--- src/components/color-randomizer/color-randomizer.jsx
+++ src/components/color-randomizer/color-randomizer.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 export default class ColorRandomizer extends Component {
+  state = {
+    selectedColor: 'green'
+  }
+
   render() {
     const colors = ['green', 'pink', 'orange'];
     return (
       <>
         {
           colors.map((color) => (
-            <button key={color}>{color}</button>
-          ))}
+            <button key={color} style={{ backgroundColor: color }}>
+              {color}</button>
+          ))
+        }
       </>
     );
   }
